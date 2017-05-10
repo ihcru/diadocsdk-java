9a3e7c08c4eb309812ef244a1503aab93c73124d
xaos <vladimir.p.polyakov@gmail.com>
2017-05-10 20:33:29 +0500

    работа без эцп

--- src/main/java/Diadoc/Api/DiadocApi.java
+++ src/main/java/Diadoc/Api/DiadocApi.java
@@ -6,17 +6,11 @@ import Diadoc.Api.Proto.Documents.*;
 import Diadoc.Api.Proto.Events.DiadocMessage_GetApiProtos;
 import Diadoc.Api.Proto.Events.DiadocMessage_PostApiProtos;
 import Diadoc.Api.Proto.Invoicing.*;
-import Diadoc.Api.Proto.Invoicing.Signers.*;
+import Diadoc.Api.Proto.Invoicing.Signers.ExtendedSignerProtos;
 import Diadoc.Api.Proto.Recognition.RecognitionProtos;
 import com.google.gson.Gson;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.MessageLite;
-import com.objsys.asn1j.runtime.Asn1BerDecodeBuffer;
-import com.objsys.asn1j.runtime.Asn1BerEncodeBuffer;
-import com.objsys.asn1j.runtime.Asn1Exception;
-import com.objsys.asn1j.runtime.Asn1Type;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.StringUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.*;
 import org.apache.http.auth.AuthScope;
@@ -32,20 +26,8 @@ import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.*;
-import ru.CryptoPro.Crypto.CryptoProvider;
-import ru.CryptoPro.Crypto.spec.GostCipherSpec;
-import ru.CryptoPro.JCP.ASN.CryptographicMessageSyntax.ContentInfo;
-import ru.CryptoPro.JCP.ASN.CryptographicMessageSyntax.EnvelopedData;
-import ru.CryptoPro.JCP.ASN.CryptographicMessageSyntax.KeyTransRecipientInfo;
-import ru.CryptoPro.JCP.ASN.CryptographicMessageSyntax.RecipientInfo;
-import ru.CryptoPro.JCP.ASN.Gost28147_89_EncryptionSyntax.Gost28147_89_Parameters;
-import ru.CryptoPro.JCP.ASN.GostR3410_EncryptionSyntax.GostR3410_KeyTransport;
-import ru.CryptoPro.JCP.JCP;
-import ru.CryptoPro.JCP.params.CryptParamsSpec;
+import org.apache.http.params.HttpProtocolParams;
 
-import javax.crypto.*;
-import javax.crypto.spec.IvParameterSpec;
 import javax.mail.internet.ContentDisposition;
 import javax.mail.internet.ParseException;
 import javax.net.ssl.SSLContext;
@@ -55,11 +37,10 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.security.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.security.spec.AlgorithmParameterSpec;
-import java.security.spec.X509EncodedKeySpec;
 import java.util.*;
 import java.util.concurrent.TimeoutException;
 
@@ -175,14 +156,6 @@ public class DiadocApi {
         updateCredentials(result);
     }
 
-    public void Authenticate(X509Certificate currentCert) throws Exception {
-        updateCredentials(null);
-        byte[] responseBody = PerformPostHttpRequest("/Authenticate", null, currentCert.getEncoded());
-        String authToken = getAuthToken(responseBody, currentCert);
-        // System.out.println(authToken);
-        updateCredentials(authToken);
-    }
-
     private void updateCredentials(String authToken) {
         authenticated = (authToken != null);
         DiadocCredentials diadocCredentials = new DiadocCredentials(apiClientId, authToken);
@@ -218,68 +191,6 @@ public class DiadocApi {
         if (documentId != null)
             params.add(new BasicNameValuePair("documentId", documentId));
         PerformPostHttpRequest("/Delete", params, new byte[]{});
-    }
-
-    private String getAuthToken(byte[] encryptedToken, X509Certificate currentCert) throws Exception {
-        byte[] decryptedToken = decryptToken(encryptedToken, currentCert);
-        return StringUtils.newStringUtf8(Base64.encodeBase64(decryptedToken));
-    }
-
-    private void decodeAsn1Bytes(Asn1Type obj, byte[] bytes) throws IOException, Asn1Exception {
-        Asn1BerDecodeBuffer dbuf = new Asn1BerDecodeBuffer(bytes);
-        obj.decode(dbuf);
-    }
-
-    private KeyTransRecipientInfo getKeyTransRecipientInfo(EnvelopedData cms) throws Exception {
-        for (RecipientInfo recipientInfo: cms.recipientInfos.elements) {
-            if (recipientInfo.getChoiceID() == RecipientInfo._KTRI)
-                return (KeyTransRecipientInfo)recipientInfo.getElement();
-        }
-        throw new Exception("KeyTransRecipientInfo not found");
-    }
-
-    private byte[] encodeAsn1(Asn1Type obj) throws Asn1Exception {
-        Asn1BerEncodeBuffer ebuf = new Asn1BerEncodeBuffer();
-        obj.encode(ebuf);
-        return ebuf.getMsgCopy();
-    }
-
-    private Key gostUnwrapKey(byte[] wrappedKey, Key secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
-        Cipher cipher = Cipher.getInstance(CryptoProvider.GOST_CIPHER_NAME);
-        cipher.init(Cipher.UNWRAP_MODE, secretKey);
-        return cipher.unwrap(wrappedKey, null, Cipher.SECRET_KEY);
-    }
-
-    private byte[] gostDecrypt(byte[] encryptedContent, Key simmKey, AlgorithmParameterSpec algorithmParameterSpec) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher = Cipher.getInstance(CryptoProvider.GOST_CIPHER_NAME);
-        cipher.init(Cipher.DECRYPT_MODE, simmKey, algorithmParameterSpec);
-        return cipher.doFinal(encryptedContent, 0, encryptedContent.length);
-    }
-
-    private byte[] decryptToken(byte[] encryptedToken, X509Certificate currentCert) throws Exception {
-        ContentInfo contentInfo = new ContentInfo();
-        decodeAsn1Bytes(contentInfo, encryptedToken);
-        EnvelopedData cms = (EnvelopedData) contentInfo.content;
-        KeyTransRecipientInfo recipientInfo = getKeyTransRecipientInfo(cms);
-        GostR3410_KeyTransport encrKey = new GostR3410_KeyTransport();
-        decodeAsn1Bytes(encrKey, recipientInfo.encryptedKey.value);
-        byte[] encodedPub = encodeAsn1(encrKey.transportParameters.ephemeralPublicKey);
-        X509EncodedKeySpec pspec = new X509EncodedKeySpec(encodedPub);
-        KeyFactory kf = KeyFactory.getInstance(JCP.GOST_DH_NAME);
-        PublicKey senderPublic = kf.generatePublic(pspec);
-        KeyAgreement responderKeyAgree = KeyAgreement.getInstance(JCP.GOST_DH_NAME);
-        byte[] sv = encrKey.transportParameters.ukm.value;
-        PrivateKey privateKey = CertificateHelper.getPrivateKey(currentCert, null);
-        responderKeyAgree.init(privateKey, new IvParameterSpec(sv));
-        responderKeyAgree.doPhase(senderPublic, true);
-        Key responderSecret = responderKeyAgree.generateSecret(CryptoProvider.GOST_CIPHER_NAME);
-        byte[] wrappedSimmKey = encodeAsn1(encrKey.sessionEncryptedKey);
-        Key simmKey = gostUnwrapKey(wrappedSimmKey, responderSecret);
-        Gost28147_89_Parameters params = (Gost28147_89_Parameters) cms.encryptedContentInfo.contentEncryptionAlgorithm.parameters;
-        byte[] iv = params.iv.value;
-        AlgorithmParameterSpec spec = new GostCipherSpec(iv, CryptParamsSpec.getInstance());
-        byte[] encryptedContent = cms.encryptedContentInfo.encryptedContent.value;
-        return gostDecrypt(encryptedContent, simmKey, spec);
     }
 
     private URI BuildRequestURI(URI baseUri, String path, List<NameValuePair> parameters) throws URISyntaxException {
